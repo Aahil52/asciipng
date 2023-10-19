7d0594ecb4fec67289bf76c4ff394127fd0e76d8
Aahil Lakhani <lakhaniaahil@gmail.com>
2023-10-19 19:06:27 -0400

    Replace extracted with decoded in variable names

--- src/decoder.c
+++ src/decoder.c
@@ -39,13 +39,13 @@ int main(int argc, char *argv[]) {
     const size_t char_bit_size = 8;
     const size_t message_checksum_bit_size = 32;
 
-    unsigned int extracted_message_signature = 0;
+    unsigned int decoded_message_signature = 0;
     for (size_t i = 0; i < message_signature_bit_size; i += 2) {
         unsigned char crumb = image[i / 2] & 0b11;
-        extracted_message_signature |= (crumb << ((message_signature_bit_size - 2) - i));
+        decoded_message_signature |= (crumb << ((message_signature_bit_size - 2) - i));
     }
 
-    if (extracted_message_signature == defined_message_signature) {
+    if (decoded_message_signature == defined_message_signature) {
         printf("Message signature found\n");
     } else {
         printf("Message signature not present: no message encoded\n");
@@ -53,57 +53,57 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    unsigned int extracted_message_length = 0;
+    unsigned int decoded_message_length = 0;
     unsigned char expected_message_length_checksum = 0;
     size_t message_length_index = message_signature_bit_size / 2;
     for (size_t i = 0; i < message_length_bit_size; i += 2) {
         unsigned char crumb = image[message_length_index + (i / 2)] & 0b11;
-        extracted_message_length |= (crumb << ((message_length_bit_size - 2) - i));
+        decoded_message_length |= (crumb << ((message_length_bit_size - 2) - i));
         expected_message_length_checksum += crumb;
     }
 
-    unsigned char extracted_message_length_checksum = 0;
+    unsigned char decoded_message_length_checksum = 0;
     size_t message_length_checksum_index = message_length_index + (message_length_bit_size / 2);
     for (size_t i = 0; i < message_length_checksum_bit_size; i += 2) {
         unsigned char crumb = image[message_length_checksum_index + (i / 2)] & 0b11;
-        extracted_message_length_checksum |= (crumb << ((message_length_checksum_bit_size - 2) - i));
+        decoded_message_length_checksum |= (crumb << ((message_length_checksum_bit_size - 2) - i));
     }
 
-    if (extracted_message_length_checksum == expected_message_length_checksum) {
-        printf("Message length successfully decoded (%u)\n", extracted_message_length);
+    if (decoded_message_length_checksum == expected_message_length_checksum) {
+        printf("Message length successfully decoded (%u)\n", decoded_message_length);
     } else {
         printf("Error decoding message length: checksum invalid\n");
         free(image);
         return 1;
     }
 
-    char extracted_message[extracted_message_length + 1];
-    extracted_message[extracted_message_length] = '\0';
+    char decoded_message[decoded_message_length + 1];
+    decoded_message[decoded_message_length] = '\0';
 
     unsigned int expected_message_checksum = 0;
     size_t message_index = message_length_checksum_index + (message_length_checksum_bit_size / 2);
-    for (size_t i = 0; i < extracted_message_length; i++) {
-        char extracted_char = 0;
+    for (size_t i = 0; i < decoded_message_length; i++) {
+        char decoded_char = 0;
         for (size_t j = 0; j < char_bit_size; j += 2) {
             unsigned char crumb = image[message_index + (((i * char_bit_size) + j) / 2)] & 0b11;
-            extracted_char |= (crumb << ((char_bit_size - 2) - j));
+            decoded_char |= (crumb << ((char_bit_size - 2) - j));
             expected_message_checksum += crumb;
         }
-        extracted_message[i] = extracted_char;
+        decoded_message[i] = decoded_char;
     }
-    printf("%s\n%u\n", extracted_message, expected_message_checksum);
+    printf("%s\n%u\n", decoded_message, expected_message_checksum);
 
-    unsigned char extracted_message_checksum = 0;
-    size_t message_checksum_index = message_index + ((char_bit_size * extracted_message_length) / 2);
+    unsigned char decoded_message_checksum = 0;
+    size_t message_checksum_index = message_index + ((char_bit_size * decoded_message_length) / 2);
     for (size_t i = 0; i < message_checksum_bit_size; i += 2) {
         unsigned char crumb = image[message_checksum_index + (i / 2)] & 0b11;
-        extracted_message_checksum |= (crumb << ((message_checksum_bit_size - 2) - i));
+        decoded_message_checksum |= (crumb << ((message_checksum_bit_size - 2) - i));
     }
 
-    if (extracted_message_checksum == expected_message_checksum) {
-        printf("Message successfully decoded:\n%s\n", extracted_message);
+    if (decoded_message_checksum == expected_message_checksum) {
+        printf("Message successfully decoded:\n%s\n", decoded_message);
     } else {
-        printf("Error decoding message: checksum invalid\n%u\n", extracted_message_checksum);
+        printf("Error decoding message: checksum invalid\n%u\n", decoded_message_checksum);
     }
 
     free(image);
